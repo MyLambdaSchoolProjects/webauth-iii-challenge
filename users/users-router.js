e9d27eb1d18f736e9ab7bf305d914752a4301d21
David Isakson <ikeman32@yahoo.com>
2019-12-18 13:31:21 -0800

    First commit

---
+++ users/users-router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const Users = require("./users-model");
+
+const restricted = require('../auth/restricted-middleware.js');//for basic authorization
+
+router.get("/", (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
