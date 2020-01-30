b13af91eadf8ae0c4437be5511d821541ccd8cc4
David Isakson <ikeman32@yahoo.com>
2020-01-30 13:05:11 -0800

    sretch

--- users/users-router.js
+++ users/users-router.js
@@ -5,13 +5,22 @@ const Users = require("./users-model");
 const restricted = require('../auth/restricted-middleware.js');//for basic authorization
 
 router.get("/", restricted, (req, res) => {
+  const dept = req.token.department
+  console.log(dept);
   Users.find()
     .then(users => {
-      res.status(200).json(users);
+      res.status(200).json(users.filter(user=> user.department === dept));
+      
     })
     .catch(err => {
       res.status(500).json(err);
     });
 });
+router.get("/all", restricted, (req, res) =>{
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+})
 
 module.exports = router;
